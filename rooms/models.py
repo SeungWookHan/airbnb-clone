28663b4a9482b107bc07d8ae982c93fa189dab3c
SeungWookHan <hswook12@me.com>
2021-01-02 20:55:08 +0900

    Add rooms Model & Admin

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
 
-# Create your models here.
+# from users import models as user_models
+
+
+class AbstrctItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstrctItem):
+    """ RoomType Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Room Types"
+
+
+class Amenity(AbstrctItem):
+    """ Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstrctItem):
+    """ Facility Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstrctItem):
+    """ HouseRule Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "House Rules"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definiation """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
