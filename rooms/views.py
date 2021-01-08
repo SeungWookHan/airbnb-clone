e1df1cfd96321059e511d84aef254b81254a238e
SeungWookHan <hswook12@me.com>
2021-01-09 01:58:54 +0900

    Rooms Template & HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
