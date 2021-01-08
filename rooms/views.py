80b8c09bc4058d262979aef6effd5abcf24998d6
SeungWookHan <hswook12@me.com>
2021-01-09 03:47:54 +0900

    Detail View Complete

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -13,9 +11,8 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def room_detail(request, pk):
-        try:
-            room = models.Room.objects.get(pk=pk)
-            return render(request, "rooms/detail.html", {"room": room})
-        except models.Room.DoesNotExist:
-            return redirect(reverse("core:home"))
+
+class RoomDetail(DetailView):
+    """ Room Detail Definition"""
+
+    model = models.Room
